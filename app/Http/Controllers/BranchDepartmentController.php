d70091e3576491db8e3b84b6bb34e771329a3e0f
eardakbekov1 <eardakbekov@gmail.com>
2023-04-15 18:23:59 +0600

    Migrations are done

---
+++ app/Http/Controllers/BranchDepartmentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Branch_department;
+use Illuminate\Http\Request;
+
+class BranchDepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Branch_department  $branch_department
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Branch_department $branch_department)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Branch_department  $branch_department
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Branch_department $branch_department)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Branch_department  $branch_department
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Branch_department $branch_department)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Branch_department  $branch_department
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Branch_department $branch_department)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_04_11_203632_create_histories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('histories', function (Blueprint $table) {
+            $table->id();
+            $table->string('handovered_date');
+            $table->string('received_date')->nullable();
+            $table->integer('device_id');
+            $table->integer('employee_id');
+            $table->string('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('histories');
+    }
+}
